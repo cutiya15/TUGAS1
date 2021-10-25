d31c3b1679bb3d034f9eff94001e0a656c0e778b
cutiya15 <72597688+cutiya15@users.noreply.github.com>
2021-10-25 09:51:10 +0700

    Add files via upload

---
+++ kondisi if else.php
@@ -0,0 +1,9 @@
+<?php 
+	$Memasak = "Cutiya";
+	if ($Memasak == "Cutiya") {
+		echo "Cutiya hobi memasak";
+	}
+	else{
+		echo "Cutiya tidak hobi memasak";
+	}
+?>
---
+++ variable.php
@@ -0,0 +1,8 @@
+<?php 
+$nama="Cutiya";
+$alamat="Pekanbaru";
+$agama="islam";
+$umur= "20";
+$coba_coba = "hobi saya senang membaca novel dan juga saat ini saya sedang berkuliah di malang, nama kampus saya UNIVERSITAS GAJAYANA MALANG ";
+echo "Hallo everyone, perkenalkan nama saya $nama saat ini saya tinggal di $alamat saya beragama $agama, dan aku baru saja memasuki $umur tahun. <br> $coba_coba;
+?>
